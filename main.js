82b548a14ddf4f23364e22cebaba267d1248c765
Gabriele <gabrielepese@gmail.com>
2020-07-03 17:17:20 +0200

    finito!!!

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
 $(document).ready(function(){
-// PARTE KEYBOARD
 
+// PARTE KEYBOARD
+$(document).keydown(function() {
+    var tasto = event.which
+    if (tasto == 39) {var imgActive = $(".slider-wrapper img.active")
+      imgActive.removeClass ("active");
+
+
+      if (imgActive.hasClass("last") == true) {
+        $(".slider-wrapper img.first")  .addClass("active");
+      }
+    else {
+        imgActive.next().addClass("active");
+    }
+    var iActive = $(".slider-wrapper i.active")
+      iActive.removeClass ("active");
+
+
+      if (iActive.hasClass("last") == true) {
+        $(".slider-wrapper i.first")  .addClass("active");
+      }
+    else {
+        iActive.next().addClass("active");
+    }
+    }
+
+    // TASTO INDIETRO
+    else if (tasto == 37) {
+      var imgActive = $(".slider-wrapper img.active")
+        imgActive.removeClass ("active");
+
+
+        if (imgActive.hasClass("first") == true) {
+          $(".slider-wrapper img.last")  .addClass("active");
+        }
+      else {
+          imgActive.prev().addClass("active");
+      }
+      var iActive = $(".slider-wrapper i.active")
+        iActive.removeClass ("active");
+
+
+        if (iActive.hasClass("first") == true) {
+          $(".slider-wrapper i.last")  .addClass("active");
+        }
+      else {
+          iActive.prev().addClass("active");
+      }
+    }
+
+  });
 
   $(".next").click (function (){
 
